05f02537ef779fa8fd1ea9fbd4eb42d3541c3da1
tiak7 <113570458+tiak7@users.noreply.github.com>
2022-10-22 17:26:23 -0400

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -1,74 +1,77 @@
-package crossy;
+package crossy; 
 
-import java.util.Random;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.io.IOException;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+import javax.imageio.ImageIO;
 
-public class Cars {
-	private Rectangle car;
-	private int speed,x,y,w,h;
+public class Vehicles {
+	private Rectangle vehicle;
+	private int speed, x, y, w, h;
 	private BufferedImage image;
-	
-	Cars(int x,int y,int w,int h,int speed){
-		this.x=x;
-		this.y=y;
-		this.w=w;
-		this.h=h;
-		this.speed=speed;	
-		setCarType();
-		car=new Rectangle(x,y,w,h);
-	}
-	
-	public void setCarType(){
-		Random rand= new Random();
-		int temp= rand.nextInt(2);
-		if(speed>0){
-			if(temp==1)
+
+    Vehicles(int x, int y, int w, int h, int speed){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.speed = speed;	
+        setType();
+        vehicle = new Rectangle(x, y, w, h);
+    }
+
+    public void setType(){
+		Random random = new Random();
+		int rand = random.nextInt(2);
+		if(speed > 0){
+			if(rand == 0)
 				changeSprite("img/RamblingWreckRight.png");
 			else 
 				changeSprite("img/StingerRight.png");
 		}
 		else{
-			if(temp==1)
+			if(rand == 0)
 				changeSprite("img/StingerLeft.png");
 			else 
 				changeSprite("img/RamblingWreckLeft.png");
 		}
 	}
-	
-	public void changeSprite(String sprite){
+    public void changeSprite(String sprite){
 		try {
-			image= ImageIO.read(getClass().getResourceAsStream(sprite));
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			image = ImageIO.read(getClass().getResourceAsStream(sprite));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	public void render(Graphics g){
-		Graphics2D g2d= (Graphics2D)g;
-		g2d.drawImage(image, car.x, car.y, null);
+
+	public void graphic(Graphics g){
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.drawImage(image, vehicle.x, vehicle.y, null);
 		move();
-		if(isOut()){
-			if(car.x>500)
-				car.x=-100;
-			else if(car.x<500)
-				car.x=Display.WIDTH;
-		}
+		if(OutOfBounds()){
+			if(vehicle.x > 500)
+				vehicle.x =- 100;
+			else {
+				vehicle.x = Display.WIDTH;
+		    }
+        }
 	}
 	public void move(){
-		car.x=car.x+speed;
+		vehicle.x += speed;
 	}
-	public boolean isOut(){
-		return car.getMinX()>=Display.WIDTH||car.getMaxX()<=0;
+   
+	public boolean OutOfBounds(){
+		return vehicle.getMaxX() <= 0 || vehicle.getMinX() >= Display.WIDTH;
 	}
-	public Rectangle getCar() {
-		return car;
+ 
+	public Rectangle getVehicle() {
+		return vehicle;
 	}
-	public void setCar(Rectangle car) {
-		this.car = car;
+ 
+	public void setVehicle(Rectangle vehicle) {
+		this.vehicle = vehicle;
 	}
-	
 }
